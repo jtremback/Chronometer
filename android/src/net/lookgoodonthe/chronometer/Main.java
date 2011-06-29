1b2c14fcfa95b948728e3f0f27a810e789cc63e5
Joshua Juran <jjuran@metamage.com>
2011-06-28 17:58:57 -0700

    Add minimal Android application

---
+++ android/src/net/lookgoodonthe/chronometer/Main.java
@@ -0,0 +1,19 @@
+package net.lookgoodonthe.chronometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public final class Main extends Activity
+{
+	
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		
+		setContentView( R.layout.main );
+	}
+	
+}
+
