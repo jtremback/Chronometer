487cc94bc16a7869da6b73c4aa87febdd6e5898e
Josh Juran <jjuran@metamage.com>
2012-08-06 04:10:10 -0700

    Make the alarm and timer handles draggable

--- src/ClockView.java
+++ src/ClockView.java
@@ -42,6 +42,11 @@ public class ClockView extends View
 	
 	private int dragging = NOTHING;
 	
+	private double initialDragAngle;
+	private double lastDragAngle;
+	
+	private int crossings;
+	
 	private Handler updateHandler = new Handler();
 	
 	private Runnable update = new Runnable()
@@ -185,8 +190,10 @@ public class ClockView extends View
 		
 		final float secondAngle = seconds *  6;  // 360 / 60
 		
-		alarmAngle = hourAngle;
-		timerAngle = minuteAngle;
+		final float dragAngle = (float) lastDragAngle;
+		
+		alarmAngle = dragging == ALARM_HANDLE  &&  crossings >= 0 ? dragAngle : hourAngle;
+		timerAngle = dragging == TIMER_HANDLE  &&  crossings >= 0 ? dragAngle : minuteAngle;
 		
 		drawHand( canvas, hourHand,    hourAngle   );
 		drawHand( canvas, alarmHandle, alarmAngle  );
@@ -222,6 +229,32 @@ public class ClockView extends View
 		vibrator.vibrate( 8 );
 		
 		return true;
+	}
+	
+	private void beginDrag()
+	{
+		initialDragAngle = dragging == ALARM_HANDLE ? alarmAngle : timerAngle;
+		
+		crossings = 0;
+		
+		lastDragAngle = initialDragAngle;
+	}
+	
+	private void updateDrag( double angle )
+	{
+		final double a = Trig.signedAngularDistance( initialDragAngle, lastDragAngle );
+		final double b = Trig.signedAngularDistance( initialDragAngle, angle         );
+		
+		final double c = Trig.signedAngularDistance( angle, lastDragAngle );
+		
+		if ( Math.abs( b ) < 90  &&  (a < 0) != (b < 0) )
+		{
+			crossings += a < b ? 1 : -1;
+		}
+		
+		lastDragAngle = angle;
+		
+		invalidate();
 	}
 	
 	@Override
@@ -265,10 +298,14 @@ public class ClockView extends View
 			
 			dragging = hit;
 			
+			beginDrag();
+			
 			return hitFeedback();
 		}
 		else  // MOVE or UP
 		{
+			updateDrag( angle );
+			
 			if ( action == MotionEvent.ACTION_UP )
 			{
 				dragging = NOTHING;
