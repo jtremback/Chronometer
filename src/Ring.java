88378f95c7df62461f56a60e55fcdc3154521dcc
Josh Juran <jjuran@metamage.com>
2012-09-16 13:24:53 -0700

    Move alarm vibration to new Ring activity

---
+++ src/Ring.java
@@ -0,0 +1,43 @@
+package com.metamage.chronometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.WindowManager;
+
+
+public final class Ring extends Activity
+{
+	
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		
+		getWindow().addFlags( + WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+		                      | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+		                      | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+		                      | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+		
+		vibrate( 500 );
+		
+		finish();
+	}
+	
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		
+		overridePendingTransition( 0, 0 );
+	}
+	
+	public void vibrate( long ms )
+	{
+		Vibrator vibrator = (Vibrator) getSystemService( VIBRATOR_SERVICE );
+		
+		vibrator.vibrate( ms );
+	}
+	
+}
+
